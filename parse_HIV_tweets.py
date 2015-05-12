cd3c7c1356e8e300e6ab6bb938c2bd413c9153ce
Dong Jin Yu <yudj0604@gmail.com>
2015-05-12 02:55:56 -0700

    rdy to pull

---
+++ parse_HIV_tweets.py
@@ -0,0 +1,46 @@
+import sys
+import csv
+
+def parse_and_write(outfile_name):
+    with open('tweets/HIV_0001_1000.csv', 'rU') as infile:
+        hiv_reader = csv.reader(infile, delimiter=',')
+        with open(outfile_name, 'wb') as outfile:
+            hiv_writer = csv.writer(outfile, delimiter=',')
+            hiv_writer.writerow(['id', 'text', 'HIV'])
+            st = 0
+            for row_in in hiv_reader:
+                if(st==0): #ignore the first row
+                    st = 1
+                else:
+                    row_vec = [row_in[9], row_in[28], row_in[39]] #use Sean's classification; 39th column
+                    hiv_writer.writerow(row_vec)
+    
+    with open('tweets/HIV_1001_5000.csv', 'rU') as infile:
+        hiv_reader = csv.reader(infile, delimiter=',')
+        with open(outfile_name, 'a') as outfile:
+            hiv_writer = csv.writer(outfile, delimiter=',')
+            st=0
+            for row_in in hiv_reader:
+                if(st==0):
+                    st = 1
+                else:
+                    if(row_in[2] != ""): #ignore rows that are not classified
+                        hiv_writer.writerow(row_in)
+                        
+    with open('tweets/HIV_5001_7000.csv', 'rU') as infile:
+        hiv_reader = csv.reader(infile, delimiter=',')
+        with open(outfile_name, 'a') as outfile:
+            hiv_writer = csv.writer(outfile, delimiter=',')
+            st=0
+            for row_in in hiv_reader:
+                if(st==0):
+                    st = 1
+                else:
+                    row_vec = [row_in[0], row_in[1], row_in[2]] #only retrieve id, text, hiv columns
+                    hiv_writer.writerow(row_vec)
+
+if __name__ == "__main__":
+    if len(sys.argv)>1:
+        parse_and_write(sys.argv[1])
+    else:
+        print("Give a file name")
